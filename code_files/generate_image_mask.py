e1ff8b8bd47d22d7d814efe9265abffffa1753a8
bikram11 <bekrumed1@gmail.com>
2022-11-10 17:19:32 -0500

    Handling mask generation for clipped  vehicle

--- code_files/generate_image_mask.py
+++ code_files/generate_image_mask.py
@@ -41,12 +41,15 @@ for inddir in dir_list:
         ret,frame = cap.read()
         if ret == True:
             # bounding box for leading vehicle
-            start_point = (int(data['sequence'][i]['TgtXPos_LeftUp']),int(data['sequence'][i]['TgtYPos_LeftUp']))
-            end_point = (int(data['sequence'][i]['TgtXPos_LeftUp']+data['sequence'][i]['TgtWidth']),int(data['sequence'][i]['TgtYPos_LeftUp']+data['sequence'][i]['TgtHeight']))
+            start_x_point = 0 if int(data['sequence'][i]['TgtXPos_LeftUp']) < 0 else int(data['sequence'][i]['TgtXPos_LeftUp'])
+            start_y_point = 0 if int(data['sequence'][i]['TgtYPos_LeftUp']) < 0 else int(data['sequence'][i]['TgtYPos_LeftUp'])
+            start_point = (start_x_point,start_y_point)
+            end_point = (int(start_x_point+data['sequence'][i]['TgtWidth']),int(start_y_point+data['sequence'][i]['TgtHeight']))
             color = (255,0,0)
             # img = cv2.imread('image1.png') # read image
             mask = np.zeros((frame.shape[0],frame.shape[1]),dtype=np.uint8) # initialize mask
             mask[start_point[1]:end_point[1],start_point[0]:end_point[0]] = 255 # fill with white pixels
+            print(maskedImagePath+str(i)+'_mask.png')
             cv2.imwrite(maskedImagePath+str(i)+'_mask.png',mask) # save mask
             # frame = cv2.rectangle(frame, start_point,end_point,color,2)
 
@@ -63,7 +66,7 @@ for inddir in dir_list:
             # text = 'Leading  Vehicle Speed : %s'%(data['sequence'][i]['TgtSpeed_ref'] )
             # frame = cv2.putText(frame, text, (10,110), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 1, cv2.LINE_AA )
 
-
+            i+=1
             cv2.imshow('Frame',frame)
 
             if cv2.waitKey(25)& 0xFF == ord('q'):
